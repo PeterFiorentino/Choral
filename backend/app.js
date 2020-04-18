26179fb236562b95dd1e2dd49c61655e4397b387
PeterFiorentino <peterfiorentino@pursuit.org>
2020-04-18 10:41:47 -0400

    multer

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const multer = require('multer');
+const cors = require('cors');
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) =>{
+        cb(null,"./public/images")
+    },
+    filename: (req,file,cb) =>{
+        let name = Date.now() + "-" + file.originalname
+        cb(null,name)
+    }
+
+})
+const upload = multer(
+    {
+        storage: storage
+    })
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,7 +35,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/seessions', sessionsRouter);
-app.use('./collaborations', collaborationsRouter);
+app.use('/sessions', sessionsRouter);
+app.use('/collaborations', collaborationsRouter);
+
+app.post('/upload',upload.single("audio"),(req,res,next) =>{
+    console.log("file",req.file) 
+    console.log("body",req.body)
+    let audioURL = "http://localhost:3001" + req.file.path.replace('public','')
+    res.json({
+        message: "file uploaded",
+        audioUrl: audioURL
+    })
+} )
 
 module.exports = app;
