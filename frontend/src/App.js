e08ed67c6e05adc047892bec15596bf5c36dd520
Owen Jones <owenthefirst93@gmail.com>
2020-04-17 22:45:07 -0400

    added more components and fixed routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom'
 import NavBar from './Components/NavBar'
 import LandingPage from './Components/LandingPage'
-// import ProfilePage from './Components/ProfilePage'
+import ProfilePage from './Components/ProfilePage'
+import Sessions from './Components/Sessions'
+import Collaborators from './Components/Collaborators'
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,6 +17,9 @@ function App() {
       <div>
         <Switch>
           <Route exact path = "/" component= {LandingPage}/>
+          <Route path = "/profile" render = {() => <ProfilePage /> } />
+          <Route path = "/sessions" render = {() =>  <Sessions /> } />
+          <Route path = "/collaborators" render = { () => <Collaborators />} />
         </Switch>
       </div>
       
---
+++ frontend/src/Components/ProfilePage.jsx
@@ -0,0 +1,19 @@
+import React,{Component} from 'react'
+
+class ProfilePage extends Component {
+    constructor(){
+        super()
+
+    }
+    render(){
+        return(
+            <div>
+                <p>Profile Page</p>
+            </div>
+        )
+    }
+}
+
+
+
+export default ProfilePage
