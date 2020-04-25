e47fee37ca94534a8c3516f9c578c10365171c2f
Owen Jones <owenthefirst93@gmail.com>
2020-04-25 17:46:14 -0400

    explore component and fetching and rendering sessionData

---
+++ frontend/src/Components/Feed/FeedContainer.jsx
@@ -0,0 +1,61 @@
+import React, {Component} from 'react'
+import NavBar from '../NavBar/NavBar.jsx'
+import MyFeed from '../Feed/MyFeed.jsx'
+import Explore from '../Feed/Explore.jsx'
+import './FeedContainer.css'
+
+class FeedContainer extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            userId : '',
+            myFeed: false,
+            explore: true,
+        }
+    }
+    
+   toggleMyFeed = () => {
+        this.setState({
+            myFeed: !this.state.myFeed
+        })
+   }
+
+   toggleExplore = () => {
+        this.setState({
+            explore: !this.state.explore
+        })
+   }
+
+    render(){
+        const {myFeed, explore} = this.state 
+        let renderMyFeed = myFeed ? (
+            <div>
+                <h2>My Feed</h2>
+                <MyFeed />
+            </div>
+        ): null
+        let renderExplorePage = explore ? (
+            <div>
+                <h2>Explore</h2>
+                <Explore />
+            </div>
+        ):null
+
+        return(
+            <div>
+                <NavBar />
+                <h1>Feed</h1>
+                    <button onClick={this.toggleMyFeed}>My Feed</button>
+                    {renderMyFeed}
+                    <button onClick={this.toggleExplore}>Explore</button>
+                    {renderExplorePage}
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default FeedContainer
