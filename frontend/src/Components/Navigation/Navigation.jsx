5f9ab694a5af005360f338bd65084a0b80c1b722
Owen Jones <owenthefirst93@gmail.com>
2020-04-26 17:22:27 -0400

    linked icons and buttons and component org

--- frontend/src/Components/Navigation/Navigation.jsx
+++ frontend/src/Components/Navigation/Navigation.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Drawer, List, ListItem, ListItemIcon, ListItemText} from "@material-ui/core/"
+import {Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton} from "@material-ui/core/"
 import {makeStyles} from "@material-ui/core/styles"
+import HomeSharpIcon from '@material-ui/icons/HomeSharp';
+import AccountCircleSharpIcon from '@material-ui/icons/AccountCircleSharp';
+import GroupWorkSharpIcon from '@material-ui/icons/GroupWorkSharp';
+import ListSharpIcon from '@material-ui/icons/ListSharp';
+import ExitToAppSharpIcon from '@material-ui/icons/ExitToAppSharp';
 import './Navigation.css'
 
 const useStyles = makeStyles((theme) => ({
-    drawerPaper : { width: 'inherit'},
+    drawerPaper : { 
+        width: 'inherit',
+        
+    },
+    link: {
+        textDecoration: 'none',
+        color: theme.palette.text.primary
+    },
     
 }))
 
@@ -21,12 +33,54 @@ const Navigation = () => {
                     open = {true}
                     classes={{paper: classes.drawerPaper}}
                 >
-                <Link to= "/landing"> Home </Link>{" "}
-                <Link to= "/profile"> Profile Page </Link>{" "}
-                <Link to= "/collaborators"> Collaborators  </Link>{" "}
-                <Link to= "/feed"> Feed </Link>{" "}
-                <Link to= "/landing"> Log out </Link>{" "}
+                    <List>
+                        <Link to= "/landing"> 
+                        <ListItem button>
+                            <ListItemIcon >
+                                <HomeSharpIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={"Home"} />
+                        </ListItem>
+                       </Link>{" "}
 
+                       <Link to= "/profile">
+                        <ListItem button>
+                            <ListItemIcon>
+                                <AccountCircleSharpIcon />
+                            </ListItemIcon> 
+                            <ListItemText primary={"Profile"}/>
+                        </ListItem>
+                        </Link>{" "}
+
+                        <Link to= "/collaborators">
+                        <ListItem button>
+                            <ListItemIcon>
+                                <GroupWorkSharpIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={"Collaborators"}  />
+                        </ListItem>
+                        </Link>{" "}
+
+                        <Link to= "/feed">
+                        <ListItem>
+                            <ListItemIcon>
+                                <ListSharpIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={"Feed"}  />
+                        </ListItem>
+                        </Link>{" "}
+
+                        <Link to= "/landing">
+                        <ListItem>
+                            <ListItemIcon>
+                                <ListItemIcon>
+                                    <ExitToAppSharpIcon />
+                                </ListItemIcon>
+                            </ListItemIcon>
+                            <ListItemText primary={"Log Out"} />
+                        </ListItem>
+                        </Link>{" "} 
+                    </List>
                 </Drawer>
             </nav>
         </div>
