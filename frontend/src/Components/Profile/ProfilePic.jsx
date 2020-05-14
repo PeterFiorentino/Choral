a50007febfb50eda5a78234529129b019d58587a
cohen-salama <sergiocohensalama@pursuit.org>
2020-05-14 00:32:22 -0400

    delete session in post component

--- frontend/src/Components/Profile/ProfilePic.jsx
+++ frontend/src/Components/Profile/ProfilePic.jsx
@@ -2,12 +2,6 @@ import React from 'react'
 
 
 const ProfilePic = (props) => {
-    
-    if (props.displayedUser.avatar === ''){
-        return (
-            <img src= {`https://cdn.clipart.email/60dc0ace75203466caef979a0a5c4e24_17685-earphones-icon-cliparts-stock-vector-and-royalty-free-_450-450.jpeg`} height='300px' width= '300px'/>
-        ) 
-    }
     return (
         <img src={props.displayedUser.avatar} height='300px' width= '300px'></img>
     )
