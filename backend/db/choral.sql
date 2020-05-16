b3ee17e9ae630563a760cf97612ba4eaa930a557
PeterFiorentino <peterfiorentino@pursuit.org>
2020-05-15 23:46:35 -0400

    page should work in theory

--- backend/db/choral.sql
+++ backend/db/choral.sql
@@ -37,6 +37,7 @@ CREATE TABLE collaborations (
     id SERIAL PRIMARY KEY,
     collaborator_id INT REFERENCES users(id),
     session_id INT REFERENCES sessions(id),
+    session_owner_id INT REFERENCES users(id),
     audio VARCHAR,
     instrument_name VARCHAR,
     approved BOOLEAN,
@@ -54,8 +55,8 @@ CREATE TABLE follows (
 
 INSERT INTO users (username, email, password, avatar, location, instrument, fav_genre, anthem)
     VALUES ('Pete', 'peterfiorentino@pursuit.org', 'abc123', 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/82291899_2917625578250639_4847832305433051136_o.jpg?_nc_cat=102&_nc_sid=dd9801&_nc_oc=AQn4JLNrAp30D7yg_W_kFhZqDDsvz_2AH-r4wYj_yueD6TK1CRVIJ79ld1dvoADIuAGUccFzCkezP0On6Osbs1ub&_nc_ht=scontent-lga3-1.xx&oh=28941a3440125202e788c74887b1eb2e&oe=5EC1780E', 'Staten Island, NY', 'Piano', 'Hip-Hop', 'I Want You Back by The Jackson 5'),
-           ('Sergio', 'sergiocohensalama@pursuit.org', 'abc123', 'https://f4.bcbits.com/img/0006520542_10.jpg', 'Brooklyn, NY', 'Bass', 'Acid Jazz', 'Coronus the Terminator by Flying Lotus'),
-           ('Owen', 'owenjones@pursuit.org', 'abc123', 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/68651886_3598543233505261_6173724632814714880_o.jpg?_nc_cat=108&_nc_sid=85a577&_nc_oc=AQngpc5LQJWLalKfc4YVDei2AcYDZ1Q_Bv4-Jb0WNenPJg3G2Qi6P_Py5NuCvUzX2tD3LXHHz_U94s7iUFR7wliU&_nc_ht=scontent-lga3-1.xx&oh=c683d319bb8ce8d2681d06a977a6b5e5&oe=5EBF55A3', 'Philadelphia, PA', 'Guitar', 'Rock', 'Sailors Tale by King Crimson');
+           ('Sergio', 'sergiocohensalama@pursuit.org', 'abc123', 'https://f4.bcbits.com/img/0006520542_10.jpg','Philadelphia, PA', 'Guitar', 'Rock', 'Sailors Tale by King Crimson'),
+           ('Owen', 'owenjones@pursuit.org', 'abc123', 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/68651886_3598543233505261_6173724632814714880_o.jpg?_nc_cat=108&_nc_sid=85a577&_nc_oc=AQngpc5LQJWLalKfc4YVDei2AcYDZ1Q_Bv4-Jb0WNenPJg3G2Qi6P_Py5NuCvUzX2tD3LXHHz_U94s7iUFR7wliU&_nc_ht=scontent-lga3-1.xx&oh=c683d319bb8ce8d2681d06a977a6b5e5&oe=5EBF55A3', 'Brooklyn, NY', 'Bass', 'Acid Jazz', 'Coronus the Terminator by Flying Lotus');
 
 
 INSERT INTO sessions (owner_id, session_name, genre, bpm, session_key, chord_progression, looking_for, audio, art, session_closed, volume, stereo_position, is_deleted)
@@ -63,11 +64,11 @@ INSERT INTO sessions (owner_id, session_name, genre, bpm, session_key, chord_pro
            (2, '3/4 Rocker', 'rock', 120, 'E major', 'A-G-E', 'beat and bass', 'http://localhost:3001/audios/guit.mp3', 'http://localhost:3001/images/coral.jpeg', false, 80, 50, false);
            
 
-INSERT INTO collaborations (collaborator_id, session_id, audio, instrument_name, approved, volume, stereo_position, is_deleted)
-    VALUES (1, 1, 'http://localhost:3001/audios/bassguitar.mp3', 'Bass', false, 80, 50, false),
-           (2, 1, 'http://localhost:3001/audios/leadguitar.mp3', 'Guitar', false, 80, 50, false),
-           (3, 1, 'http://localhost:3001/audios/horns.mp3', 'Horns', false, 80, 50, false),
-           (1, 2, 'http://localhost:3001/audios/beat.mp3', 'Drums', false, 80, 50, false);
+INSERT INTO collaborations (collaborator_id, session_id, session_owner_id, audio, instrument_name, approved, volume, stereo_position, is_deleted)
+    VALUES (1, 1, 1, 'http://localhost:3001/audios/bassguitar.mp3', 'Bass', false, 80, 50, false),
+           (2, 1, 1, 'http://localhost:3001/audios/leadguitar.mp3', 'Guitar', false, 80, 50, false),
+           (3, 1, 1, 'http://localhost:3001/audios/horns.mp3', 'Horns', false, 80, 50, false),
+           (1, 2, 2, 'http://localhost:3001/audios/beat.mp3', 'Drums', false, 80, 50, false);
 
 
 INSERT INTO follows (user_id, followed_id, active_status)
@@ -77,6 +78,3 @@ INSERT INTO follows (user_id, followed_id, active_status)
           (2,3, true),
           (3,1, true),
           (3,2, true);
-
-
-SELECT * FROM collaborations WHERE 
