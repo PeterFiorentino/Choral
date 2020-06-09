e005865511846560e988b0c7237c4d476a2ded30
cohen-salama <sergiocohensalama@pursuit.org>
2020-06-09 18:18:46 -0400

    first deploy version

--- backend/app.js
+++ backend/app.js
@@ -76,6 +76,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
@@ -185,6 +186,10 @@ app.post('/upload/image', uploader.single('image'), async (req, res, next) => {
         )
         return
     }
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
 });
 
 module.exports = app;
