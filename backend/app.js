ad7594ca6a4aaff638c6443d50084d54837cc70b
cohen-salama <sergiocohensalama@pursuit.org>
2020-06-10 09:06:04 -0400

    change join to resolve

--- backend/app.js
+++ backend/app.js
@@ -77,7 +77,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -188,8 +187,10 @@ app.post('/upload/image', uploader.single('image'), async (req, res, next) => {
     }
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../frontend/build/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "../frontend/build/index.html"));
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
