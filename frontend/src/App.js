aa4777022370a682c5b8de9f9e4b85f640e3286a
cohen-salama <sergiocohensalama@pursuit.org>
2020-06-09 23:44:41 -0400

    homepage landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -95,11 +95,11 @@ class App extends Component {
         </div>
         <div>
           <Switch>
-            <Route exact path = "/" render= {this.renderLandingPage}/>
+            {/* <Route exact path = "/" render= {this.renderLandingPage}/> */}
             <Route path = "/landing" render= {this.renderLandingPage}/>
             <PrivateRoute path = "/profile/:id" render = {this.renderProfilePage} isUserLoggedIn= {isUserLoggedIn} wasInitialized= {wasInitialized}/>
             <PrivateRoute path = "/collaborators" render = {this.renderCollaboratorsPage} isUserLoggedIn={isUserLoggedIn} wasInitialized={wasInitialized}/>
-            <PrivateRoute path = "/#/feed" render = {this.renderFeedPage } isUserLoggedIn={isUserLoggedIn} wasInitialized={wasInitialized}/>
+            <PrivateRoute path = "/feed" render = {this.renderFeedPage } isUserLoggedIn={isUserLoggedIn} wasInitialized={wasInitialized}/>
             <PrivateRoute path = "/reef/:id" render = {this.renderReefPageById} isUserLoggedIn={isUserLoggedIn} wasInitialized={wasInitialized}/>
             <PrivateRoute path = "/add" render = {this.renderAddReefPage} isUserLoggedIn={isUserLoggedIn} wasInitialized={wasInitialized}/>
           </Switch>
